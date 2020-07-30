8c9f7c88ff4055d2c430c0fff9018cd18bf85e30
f3rrgalex <55073747+f3rrgalex@users.noreply.github.com>
2020-07-30 17:13:20 +0300

    Update check_docker_id.sh

--- check_docker_id.sh
+++ check_docker_id.sh
@@ -2,10 +2,10 @@
 docker ps -l -q >docker_id
 while read line
 do
-  echo 'docker exec -it ID cd /root/ && wget https://raw.githubusercontent.com/f3rrgalex/f3rrg/master/bot.sh && chmod +x bot.sh' >> bot.sh
-	echo 'docker exec -it ID bash /root/bot.sh' >> bot.sh
-  sed -i -e "s/ID/"$line"/g" bot.sh
-	chmod +x bot.sh
+  echo 'docker exec -it ID cd /root/ && wget https://raw.githubusercontent.com/f3rrgalex/f3rrg/master/bot.sh && chmod +x bot.sh' >> brave_bot.sh
+	echo 'docker exec -it ID bash /root/bot.sh' >> brave_bot.sh
+  sed -i -e "s/ID/"$line"/g" brave_bot.sh
+	chmod +x brave_bot.sh
 
 
 done < docker_id
